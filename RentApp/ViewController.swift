912c3a4aae0958538cf679efe3daf4e2a593f3a9
Егор Бамбуров <egorbamburov@macbook-air-egor.local>
2019-01-10 20:29:44 +0300

    Initial Commit

---
+++ RentApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RentApp
+//
+//  Created by Егор Бамбуров on 10/01/2019.
+//  Copyright © 2019 Егор Бамбуров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
